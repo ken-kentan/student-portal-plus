58a12c10880a69c99b3ec46a47b6e4624d97a99c
kenkentan <ken.kentan.k2@gmail.com>
2017-06-12 16:30:28 +0900

    update TLS version

--- app/src/main/java/jp/kentan/student_portal_plus/data/shibboleth/AsyncShibbolethClient.java
+++ app/src/main/java/jp/kentan/student_portal_plus/data/shibboleth/AsyncShibbolethClient.java
@@ -77,7 +77,7 @@ public class AsyncShibbolethClient {
         this.mShibbolethData = new ShibbolethData(mContext);
 
         ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                .tlsVersions(TlsVersion.TLS_1_0)
+                .tlsVersions(TlsVersion.TLS_1_2)
                 .cipherSuites(
                         CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
                         CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA256,
