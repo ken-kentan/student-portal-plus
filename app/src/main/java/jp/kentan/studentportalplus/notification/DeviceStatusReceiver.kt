5dd1d26cd6381d678c9b5ada54f713532ebb7e41
ken-kentan <ken.kentan.k2@gmail.com>
2018-07-21 17:11:12 +0900

    add DeviceStatusReceiver

---
+++ app/src/main/java/jp/kentan/studentportalplus/notification/DeviceStatusReceiver.kt
@@ -0,0 +1,13 @@
+package jp.kentan.studentportalplus.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DeviceStatusReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            SyncScheduler.scheduleIfNeed(context)
+        }
+    }
+}
