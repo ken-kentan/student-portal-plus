844fa453ad3af9e09b21d8db48deaebf75d212ae
ken-kentan <ken.kentan.k2@gmail.com>
2019-11-19 10:58:05 +0900

    update kotlin to 1.3.60

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -8,7 +8,7 @@ object Deps {
     }
 
     object Kotlin {
-        const val version = "1.3.50"
+        const val version = "1.3.60"
 
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
 
