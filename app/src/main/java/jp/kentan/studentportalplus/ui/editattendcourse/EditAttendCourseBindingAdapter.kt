3d8576e388987e16ecdf6fdbd8c8e340e06e5a8d
ken-kentan <ken.kentan.k2@gmail.com>
2020-03-01 01:14:51 +0900

    add entities

--- app/src/main/java/jp/kentan/studentportalplus/ui/editattendcourse/EditAttendCourseBindingAdapter.kt
+++ app/src/main/java/jp/kentan/studentportalplus/ui/editattendcourse/EditAttendCourseBindingAdapter.kt
@@ -1,25 +1,11 @@
 package jp.kentan.studentportalplus.ui.editattendcourse
 
-import android.widget.AutoCompleteTextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.google.android.material.button.MaterialButton
-import jp.kentan.studentportalplus.R
 import jp.kentan.studentportalplus.data.vo.CourseColor
-import jp.kentan.studentportalplus.view.widget.MaterialArrayAdapter
 
 object EditAttendCourseBindingAdapter {
-    @JvmStatic
-    @BindingAdapter("entities")
-    fun setEntities(view: AutoCompleteTextView, entityList: List<String>) {
-        val adapter = MaterialArrayAdapter(
-            view.context,
-            R.layout.item_dropdown_menu_popup,
-            entityList
-        )
-        view.setAdapter(adapter)
-        view.filters = emptyArray()
-    }
 
     @JvmStatic
     @BindingAdapter("courseColor")
--- app/src/main/java/jp/kentan/studentportalplus/ui/welcome/notification/WelcomeNotificationViewModel.kt
+++ app/src/main/java/jp/kentan/studentportalplus/ui/welcome/notification/WelcomeNotificationViewModel.kt
@@ -1,15 +1,24 @@
 package jp.kentan.studentportalplus.ui.welcome.notification
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import jp.kentan.studentportalplus.R
 import jp.kentan.studentportalplus.data.LocalPreferences
 import jp.kentan.studentportalplus.ui.Event
 import javax.inject.Inject
 
 class WelcomeNotificationViewModel @Inject constructor(
+    context: Context,
     private val localPreferences: LocalPreferences
 ) : ViewModel() {
+
+    val lectureNotificationTypeList: List<String> =
+        context.resources.getStringArray(R.array.notification_type_lecture_name).asList()
+
+    val noticeNotificationTypeList: List<String> =
+        context.resources.getStringArray(R.array.notification_type_notice_name).asList()
 
     private val _startMainActivity = MutableLiveData<Event<Unit>>()
     val startMainActivity: LiveData<Event<Unit>>
