adfe196ec43309160a5951ad26d518a6b7d325f3
ken-kentan <ken.kentan.k2@gmail.com>
2020-03-19 23:36:10 +0900

    add TimetableShortcutActivity

---
+++ app/src/main/java/jp/kentan/studentportalplus/ui/timetable/shortcut/TimetableShortcutActivity.kt
@@ -0,0 +1,35 @@
+package jp.kentan.studentportalplus.ui.timetable.shortcut
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import jp.kentan.studentportalplus.R
+import jp.kentan.studentportalplus.ui.MainActivity
+
+class TimetableShortcutActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SHORTCUT_ID = "shortcut_timetable"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val shortcutIntent = MainActivity.createIntent(this).apply {
+            action = intent.action
+        }
+
+        val shortcut = ShortcutInfoCompat.Builder(this, SHORTCUT_ID)
+            .setIcon(IconCompat.createWithResource(this, R.mipmap.timetable_shortcut_activity))
+            .setShortLabel(getString(R.string.timetable_shortcut_label))
+            .setIntent(shortcutIntent)
+            .build()
+
+        setResult(RESULT_OK, ShortcutManagerCompat.createShortcutResultIntent(this, shortcut))
+
+        finish()
+    }
+}
+
