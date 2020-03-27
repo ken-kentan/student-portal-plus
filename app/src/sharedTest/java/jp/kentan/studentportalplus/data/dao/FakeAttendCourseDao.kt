452f246decde120199e30b8d1bb9cd5fcbdaf95c
ken-kentan <ken.kentan.k2@gmail.com>
2020-03-27 10:02:21 +0900

    fix test temporally

--- app/src/sharedTest/java/jp/kentan/studentportalplus/data/dao/FakeAttendCourseDao.kt
+++ app/src/sharedTest/java/jp/kentan/studentportalplus/data/dao/FakeAttendCourseDao.kt
@@ -1,57 +1,52 @@
 package jp.kentan.studentportalplus.data.dao
 
-import jp.kentan.studentportalplus.TestData
 import jp.kentan.studentportalplus.data.entity.AttendCourse
-import jp.kentan.studentportalplus.data.entity.AttendCourseSubject
 import jp.kentan.studentportalplus.data.vo.DayOfWeek
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 class FakeAttendCourseDao : AttendCourseDao {
 
-    companion object {
-        const val ALL_LIST_SIZE = 9
-        const val DAY_OF_WEEK_LIST_SIZE = 5
+    override fun selectAsFlow(id: Long): Flow<AttendCourse?> {
+        TODO("Not yet implemented")
     }
 
-    override fun getFlow(id: Long): Flow<AttendCourse?> = flowOf(TestData.attendCourse)
+    override fun selectAsFlow(): Flow<List<AttendCourse>> {
+        TODO("Not yet implemented")
+    }
 
-    override fun getListFlow(): Flow<List<AttendCourse>> =
-        flowOf(List(ALL_LIST_SIZE) { TestData.attendCourse })
+    override fun selectAsFlow(dayOfWeek: DayOfWeek): Flow<List<AttendCourse>> {
+        TODO("Not yet implemented")
+    }
 
-    override fun getListFlow(dayOfWeek: DayOfWeek): Flow<List<AttendCourse>> =
-        flowOf(List(DAY_OF_WEEK_LIST_SIZE) { TestData.attendCourse })
+    override fun select(id: Long): AttendCourse? {
+        TODO("Not yet implemented")
+    }
 
-    override fun getSubjectListFlow(): Flow<List<AttendCourseSubject>> =
-        flowOf(listOf(TestData.attendCourseSubject))
-
-    override fun get(id: Long): AttendCourse? = TestData.attendCourse
-
-    override fun getSubjectList(): List<AttendCourseSubject> {
-        TODO("not implemented")
+    override fun select(): List<AttendCourse> {
+        TODO("Not yet implemented")
     }
 
     override fun insert(attendCourse: AttendCourse): Long {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 
-    override fun insertAll(attendCourseList: List<AttendCourse>): List<Long> {
-        TODO("not implemented")
+    override fun insert(attendCourseList: List<AttendCourse>): List<Long> {
+        TODO("Not yet implemented")
     }
 
     override fun update(attendCourse: AttendCourse): Int {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 
     override fun delete(id: Long): Int {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 
     override fun delete(subject: String, attendType: AttendCourse.Type): Int {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 
-    override fun deleteNotInHash(type: Int, hash: List<Long>) {
-        TODO("not implemented")
+    override fun deleteNotInHash(attendType: AttendCourse.Type, hash: List<Long>) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/sharedTest/java/jp/kentan/studentportalplus/data/dao/FakeLectureInformationDao.kt
+++ app/src/sharedTest/java/jp/kentan/studentportalplus/data/dao/FakeLectureInformationDao.kt
@@ -1,36 +1,27 @@
 package jp.kentan.studentportalplus.data.dao
 
-import jp.kentan.studentportalplus.TestData
 import jp.kentan.studentportalplus.data.entity.LectureInformation
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 class FakeLectureInformationDao : LectureInformationDao {
 
-    companion object {
-        const val ALL_LIST_SIZE = 9
+    override fun selectAsFlow(id: Long): Flow<LectureInformation?> {
+        TODO("Not yet implemented")
     }
 
-    override fun getFlow(id: Long): Flow<LectureInformation?> = flowOf(TestData.lectureInfo)
-
-    override fun getListFlow(): Flow<List<LectureInformation>> =
-        flowOf(List(ALL_LIST_SIZE) { TestData.lectureInfo })
-
-    override fun updateAll(lectureInfoList: List<LectureInformation>): List<LectureInformation> {
-        return lectureInfoList
+    override fun selectAsFlow(): Flow<List<LectureInformation>> {
+        TODO("Not yet implemented")
     }
 
     override fun updateRead(id: Long): Int {
-        check(id == TestData.lectureInfo.id)
-
-        return 1
+        TODO("Not yet implemented")
     }
 
     override fun insert(lectureInfo: LectureInformation): Long {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 
     override fun deleteNotInHash(hash: List<Long>) {
-        TODO("not implemented")
+        TODO("Not yet implemented")
     }
 }
